db1eddf19377b3f61330fcffa6b190291e8f2afe
Peter Milovcik <peter.milovcik@siemens-healthineers.com>
2019-06-10 14:06:23 +0200

    New AddWeightPage with Navigation

--- Eat2LoseWeight/Eat2LoseWeight/MainPage.xaml.cs
+++ Eat2LoseWeight/Eat2LoseWeight/MainPage.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace Eat2LoseWeight
 {
@@ -12,6 +7,7 @@ namespace Eat2LoseWeight
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = new MainViewModel(Navigation);
         }
     }
 }
---
+++ Eat2LoseWeight/Eat2LoseWeight/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Eat2LoseWeight
+{
+    public class MainViewModel
+    {
+        private INavigation Navigation { get; }
+
+        public MainViewModel(INavigation navigation)
+        {
+            Navigation = navigation;
+            AddWeightCommand = new Command(async () => await AddWeight());
+        }
+
+        public Command AddWeightCommand { get; }
+
+        private async Task AddWeight() => await Navigation.PushAsync(new AddWeightPage());
+    }
+}
