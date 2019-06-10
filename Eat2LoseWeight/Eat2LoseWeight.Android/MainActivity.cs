a8c2d263587c3f60b336c99d9222b3329685581f
Peter Milovcik <peter.milovcik@siemens-healthineers.com>
2019-06-10 09:46:46 +0200

    New styles and colors for with updated icons

--- Eat2LoseWeight/Eat2LoseWeight.Android/MainActivity.cs
+++ Eat2LoseWeight/Eat2LoseWeight.Android/MainActivity.cs
@@ -1,15 +1,10 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace Eat2LoseWeight.Droid
 {
-    [Activity(Label = "Eat2LoseWeight", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Eat 2 Lose Weight", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
