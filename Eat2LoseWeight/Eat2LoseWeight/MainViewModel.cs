b09441699f9146d0279180b3ef9a9ad183b34c9e
Peter Milovcik <peter.milovcik@siemens-healthineers.com>
2019-06-14 12:42:11 +0200

    Upgrade to Xamarin.Forms 4.0

--- Eat2LoseWeight/Eat2LoseWeight/MainViewModel.cs
+++ Eat2LoseWeight/Eat2LoseWeight/MainViewModel.cs
@@ -11,10 +11,15 @@ namespace Eat2LoseWeight
         {
             Navigation = navigation;
             AddWeightCommand = new Command(async () => await AddWeight());
+            AddFoodCommand = new Command(async () => await AddFood());
         }
 
         public Command AddWeightCommand { get; }
 
+        public Command AddFoodCommand { get; }
+
         private async Task AddWeight() => await Navigation.PushAsync(new AddWeightPage());
+
+        private async Task AddFood() => await Navigation.PushAsync(new AddFoodPage());
     }
 }
