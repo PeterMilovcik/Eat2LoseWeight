705cdb9e64c0a9a1ca7b6d396773b7e363b8a83f
Peter Milovcik <peter.milovcik@siemens-healthineers.com>
2019-06-24 12:51:53 +0200

    Change to Orientation to Attribute

--- Eat2LoseWeight/Eat2LoseWeight.Android/MainActivity.cs
+++ Eat2LoseWeight/Eat2LoseWeight.Android/MainActivity.cs
@@ -1,21 +1,22 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
-using Xamarin.Forms;
 
 namespace Eat2LoseWeight.Droid
 {
-    [Activity(Label = "Eat 2 Lose Weight", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(
+        Label = "Eat 2 Lose Weight",
+        Icon = "@mipmap/icon",
+        Theme = "@style/MainTheme",
+        MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
+        ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
-
-            RequestedOrientation = Device.Idiom == TargetIdiom.Phone
-                ? ScreenOrientation.Portrait
-                : ScreenOrientation.SensorLandscape;
 
             base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.SetFlags("CollectionView_Experimental");
